d43a98a5b26100a3949ec7992768196b0818d436
Gaurav Ostwal <gaurav.ostwal@coditas.com>
2020-02-02 15:07:59 +0530

    Adding the details about projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -56,6 +56,31 @@ export class Projects extends React.PureComponent {
                     </div>
                   </div>
 
+                  <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                    <div className="resume-content">
+                      <h3 className="mb-0">Life99 :- Retiral Planner</h3>
+                      <div className="subheading mb-3">Full Stack Developer</div>
+                      <p className="description">
+                      One stop shop to plan for your future, consolidated data, account views and associated services.
+                      The project includes Banking Domain, which helps the user to 
+                      plan his future after his retirement in terms of investments. Keep track of all your benefits and personal investments such as EPF, Gratuity, NPS, Mutual etc.
+                      The user can plan to buy various schemes NPS, Mutual Funds, Annuity etc
+                      with the help of  the third party APIs. 
+                      </p>
+                      <p className="description">
+                        <span className="role-label">Role: </span>
+                        <span>
+                            Developed a highly reusable frontend using <span className="role-skill">REACT</span> and <span className="role-skill">REDUX</span> that is 
+                            well wired up with BackEnd APIs using <span className="role-skill">AXIOS</span>, 
+                            implements routing using <span className="role-skill">REACT-ROUTER</span> and <span className="role-skill">WEBPACK</span> for low size bundle. Further,
+                            implemented <span className="role-skill">REST APIs</span> in <span className="role-skill">NODEJS</span> and <span className="role-skill">EXPRESSJS</span> with <span className="role-skill">JSON WEB TOKEN</span> for authorisation and used the <span className="role-skill">NON-RELATIONAL DATABASE</span> i.e <span className="role-skill">MONGODB</span> for storing the data and querying with the help of <span className="role-skill">MONGOOSE</span> and for managing <span className="role-skill">RELATIONAL DATABASE</span> used <span className="role-skill">POSTGRESQL</span> with <span className="role-skill">SEQUELIZE</span>.
+                        </span>
+                      </p>
+                    </div>
+                    <div className="resume-date text-md-right">
+                      <span className="text-primary">Oct 2019 - Present</span>
+                    </div>
+                  </div>
                 </div>
               </section>
             </Element>
